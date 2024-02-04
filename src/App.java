0eb9677f111567a2f5a62812acff9d031b7b0cca
kathrynspeltz <kathrynspeltz@gmail.com>
2024-02-04 16:58:44 -0600

    java basics update

--- src/App.java
+++ src/App.java
@@ -1,5 +1,22 @@
+import java.util.Scanner;
+
 public class App {
+    static int age2 = 6;
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        byte  lessThan127 = 5;
+        short aNumberBelow32k = 15000;
+        int aLargeNumber = 2000000;
+        long aSuperLargeNumber = 2000000000;
+        double doubleTheValuesAsFloat = 4.1425;
+        float halfTheValuesAs = 3.14f;
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("What is your name? ");
+        String name = scanner.nextLine();
+        System.out.println("Hello " + name);
+
+
+
+        System.out.println("Hello, World! I am " + lessThan127 + age2);
     }
 }
